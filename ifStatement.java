0d7550ad558d0bd21b404e57fca2f5fe11a9a23f
codesantaisai <jathurshan@protonmail.com>
2023-09-01 10:27:17 +0530

    if and operators

---
+++ ifStatement.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//comparison operator
+
+//    int x = 2 ;
+//    int y = 5;
+//        System.out.println(x<=y);
+
+//logical operator
+//    boolean isHighIncome = true;
+//    boolean isHighCredit = false;
+//    boolean hasCriminalRecords = false;
+//
+//    boolean isEligible = (isHighCredit || isHighIncome) && !hasCriminalRecords;
+//        System.out.println(isEligible);
+        
+ //If Statements
+       int temp = 30;
+ 
+ if (temp> 30){
+     System.out.println("It's is a hot day drink plenty of water");
+ } else if (temp<20) {
+     System.out.println("it is a nice day");
+ }else {
+     System.out.println("It's cold");
+ }
+    }
+}
